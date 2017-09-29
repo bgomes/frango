25236bebfbd247246e1f67166a02b294a553b485
Bruno Gomes <brunopereiragomes@gmail.com>
2017-09-29 13:31:15 -0300

    Change settings to django-1.11.5

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -28,6 +28,7 @@ TEMPLATES = [
         ],
         'APP_DIRS': True,
         'OPTIONS': {
+            'debug': DEBUG,
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
